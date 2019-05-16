e6eae3f81dba7cc3d705950073f167152fc92c9e
Bowen <zbw14@outlook.com>
2019-05-15 17:04:22 -0700

    Change serial read to threading style

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import serial_manage
 if __name__ == "__main__":
     try:
         udp_manage.start_receive_data()
+        serial_manage.start_read_data()
         while True:
-            speed_KPH = serial_manage.read_speed()
+            speed_KPH = serial_manage.speed_KPH
             print(speed_KPH)
             udp_manage.send_data(speed_KPH, 0)
     except KeyboardInterrupt:
--- serial_manage.py
+++ serial_manage.py
@@ -1,19 +1,35 @@
 import serial
+import threading
 
+# Set up the serial port
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 # ser.open()
 
+speed_KPH = 0
+
+# The speed read function
 def read_speed():
-    raw_data = ser.readline()
-    string_data = raw_data.decode("utf-8")
-    speed_KPH = float(string_data) if string_data != "" else 0
-    return speed_KPH
+    global speed_KPH
+    while True:
+        raw_data = ser.readline()
+        string_data = raw_data.decode("utf-8")
+        speed_KPH = float(string_data) if string_data != "" else 0
+
+thread = threading.Thread(target=read_speed)
+
+# Start the read threading
+def start_read_data():
+    thread.start()
+
+# Stop the read threading
+def stop_read_data():
+    thread.join()
 
 if __name__ == "__main__":
     try:
+        start_read_data()
         while True:
-            speed_KPH = read_speed()
             print(speed_KPH)
-            print(">>>>>>>>>>>>>>>>>>>>>>>>>>")
     except KeyboardInterrupt:
+        stop_read_data
         print ("\nCtrl-C pressed.  Stopping")
